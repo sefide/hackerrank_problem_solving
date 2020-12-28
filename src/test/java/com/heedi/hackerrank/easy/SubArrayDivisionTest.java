7a19eb99eb8371ecfec465ca4716e4a1c5fdcdf7
sefide <sefide@naver.com>
2020-12-28 21:42:54 +0900

    feat: add Sub-array Division solving one more

--- src/test/java/com/heedi/hackerrank/easy/SubArrayDivisionTest.java
+++ src/test/java/com/heedi/hackerrank/easy/SubArrayDivisionTest.java
@@ -12,11 +12,28 @@ public class SubArrayDivisionTest {
 
     @Test
     void test() {
-        List<Integer> s = Arrays.asList(1,2,1,3,2);
+        int[] array = {1,2,1,3,2};
+        List s = Arrays.asList(array); // List<Integer> 불가
+//        List<Integer> s1 = Arrays.asList(array); // array가 Integer[]라면 가능
         int d = 3;
         int m = 2;
 
         System.out.println(birthday(s, d, m));
+        System.out.println(birthdayUsingArray(array, d, m));
+    }
+
+    private int birthdayUsingArray(int[] s, int d, int m) {
+        int result = 0;
+
+        for (int i = 0; i <= s.length - m; i++) {
+            int sum = Arrays.stream(s, i, i+m).sum();
+
+            if(sum == d) {
+                result++;
+            }
+        }
+
+        return result;
     }
 
     static int birthday(List<Integer> s, int d, int m) {
