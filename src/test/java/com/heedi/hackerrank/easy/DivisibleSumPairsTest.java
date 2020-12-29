f12de3deab93a84bb68805e902e55840b0622056
sefide <sefide@naver.com>
2020-12-29 22:57:11 +0900

    feat: add Divisible Sum Paris Test

---
+++ src/test/java/com/heedi/hackerrank/easy/DivisibleSumPairsTest.java
@@ -0,0 +1,24 @@
+package com.heedi.hackerrank.easy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+ */
+public class DivisibleSumPairsTest {
+
+    @Test
+    void test() {
+        int n = 6;
+        int k = 3;
+        int[] ar = {1, 3, 2, 6, 1, 2};
+
+        System.out.println(divisibleSumPairs(n, k, ar));
+    }
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int result = 0;
+
+        return result;
+    }
+}
