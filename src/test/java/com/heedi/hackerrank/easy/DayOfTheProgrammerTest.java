54199afff53e6e327b4051149d80d6992b4d40a6
sefide <sefide@naver.com>
2021-01-01 21:52:01 +0900

    feat: solve Day Of The Programmer

---
+++ src/test/java/com/heedi/hackerrank/easy/DayOfTheProgrammerTest.java
@@ -0,0 +1,31 @@
+package com.heedi.hackerrank.easy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+ */
+public class DayOfTheProgrammerTest {
+
+    @Test
+    void test() {
+        int year = 2017;
+
+        System.out.println(dayOfProgrammer(year));
+
+    }
+
+    static String dayOfProgrammer(int year) {
+        String month = "09";
+        String day;
+
+        if ((year <= 1917 && year % 4 == 0) || (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            day = "12";
+        } else {
+            day = "13";
+        }
+
+        String format = "%s.%s.%s";
+        return String.format(format, month, day, year);
+    }
+}
