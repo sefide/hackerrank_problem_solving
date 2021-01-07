2b51d694a37bcbdefbbb376ab61db135203e4d75
sefide <sefide@naver.com>
2021-01-07 22:38:41 +0900

    feat: add AngryProfessorTest class

---
+++ src/test/java/com/heedi/hackerrank/easy/AngryProfessorTest.java
@@ -0,0 +1,22 @@
+package com.heedi.hackerrank.easy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://www.hackerrank.com/challenges/angry-professor/problem
+ */
+public class AngryProfessorTest {
+
+    @Test
+    void test() {
+        int[] k = {};
+        int[] a = {};
+
+        System.out.println(angryProfessor(k, a));
+    }
+
+    static String angryProfessor(int k, int[] a) {
+
+        return "YES";
+    }
+}
