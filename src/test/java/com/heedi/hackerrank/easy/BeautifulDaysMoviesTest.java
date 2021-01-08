cb88c6b1004fd8d215b78a773216a8ca52ead9b3
sefide <sefide@naver.com>
2021-01-08 22:43:37 +0900

    feat: solve Beautiful Days At the Movies

---
+++ src/test/java/com/heedi/hackerrank/easy/BeautifulDaysMoviesTest.java
@@ -0,0 +1,33 @@
+package com.heedi.hackerrank.easy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+ */
+public class BeautifulDaysMoviesTest {
+
+    @Test
+    void test() {
+        int i = 20;
+        int j = 23;
+        int k = 6;
+
+        System.out.println(beautifulDays(i, j, k));
+    }
+
+    static int beautifulDays(int i, int j, int k) {
+        int result = 0;
+
+        for (int number = i; number <= j; number++) {
+            StringBuilder stringBuffer = new StringBuilder();
+            stringBuffer.append(number);
+            int reverse = Integer.parseInt(stringBuffer.reverse().toString());
+
+            if (((number - reverse) / (double)k) == (number - reverse) / k) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
