44b2bbfe724b25a9526198384315e094cae412a7
sefide <sefide@naver.com>
2021-01-08 22:25:53 +0900

    feat: solve Angry Professor

--- src/test/java/com/heedi/hackerrank/easy/AngryProfessorTest.java
+++ src/test/java/com/heedi/hackerrank/easy/AngryProfessorTest.java
@@ -2,6 +2,8 @@ package com.heedi.hackerrank.easy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * https://www.hackerrank.com/challenges/angry-professor/problem
  */
@@ -9,14 +11,17 @@ public class AngryProfessorTest {
 
     @Test
     void test() {
-        int[] k = {};
-        int[] a = {};
+        int k = 3;
+        int[] a = {-1, -3, 4, 2};
 
         System.out.println(angryProfessor(k, a));
     }
 
     static String angryProfessor(int k, int[] a) {
+        long attendeesNumber = Arrays.stream(a)
+                .filter(time -> time <= 0)
+                .count();
 
-        return "YES";
+        return attendeesNumber >= k ? "NO" : "YES";
     }
 }
