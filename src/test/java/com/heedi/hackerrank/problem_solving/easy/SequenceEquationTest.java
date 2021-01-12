74179f981acf7967c24b93ecab14111f161171a3
sefide <sefide@naver.com>
2021-01-12 22:29:46 +0900

    feat: add Sequence Equation Test

---
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/SequenceEquationTest.java
@@ -0,0 +1,24 @@
+package com.heedi.hackerrank.problem_solving.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * https://www.hackerrank.com/challenges/permutation-equation/problem
+ */
+public class SequenceEquationTest {
+
+    @Test
+    void test() {
+        int[] p = {};
+
+        System.out.println(Arrays.toString(permutationEquation(p)));
+    }
+
+    static int[] permutationEquation(int[] p) {
+        int[] result = new int[p.length];
+
+        return result;
+    }
+}
