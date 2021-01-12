e1d4cc495efafec2e70411d9b94848f5da3eafa8
sefide <sefide@naver.com>
2021-01-12 22:40:32 +0900

    feat: solve Sequence Equation Test

--- src/test/java/com/heedi/hackerrank/problem_solving/easy/SequenceEquationTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/SequenceEquationTest.java
@@ -2,7 +2,10 @@ package com.heedi.hackerrank.problem_solving.easy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * https://www.hackerrank.com/challenges/permutation-equation/problem
@@ -11,14 +14,22 @@ public class SequenceEquationTest {
 
     @Test
     void test() {
-        int[] p = {};
+        int[] p = {2, 3, 1};
 
         System.out.println(Arrays.toString(permutationEquation(p)));
     }
 
     static int[] permutationEquation(int[] p) {
         int[] result = new int[p.length];
+        List<Integer> list = Arrays.stream(p)
+                .boxed()
+                .collect(Collectors.toList());
 
+        for (int i = 1; i <= p.length; i++) {
+            int t = list.indexOf(i);
+            int r = list.indexOf(t);
+            result[i - 1] = r;
+        }
         return result;
     }
 }
