1285d3839e436f810b09e9ffbefa0f1f6ab6313e
sefide <sefide@naver.com>
2021-01-13 22:59:10 +0900

    feat: solve Jumping On The Clouds

---
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/JumpingOnTheCloudsTest.java
@@ -0,0 +1,33 @@
+package com.heedi.hackerrank.problem_solving.easy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+ */
+public class JumpingOnTheCloudsTest {
+
+    @Test
+    void test() {
+        int[] c = {0, 0, 1, 0, 0, 1, 1, 0};
+        int k = 2;
+
+        System.out.println(jumpingOnClouds(c, k));
+    }
+
+    static int jumpingOnClouds(int[] c, int k) {
+        int n = c.length;
+        int i = 0;
+        int e = 100;
+
+        do {
+            int r = c[(i + k) % n];
+
+            e -= r * 2 + 1;
+            i = (i + k) % n;
+        } while (i != 0);
+
+        return e;
+    }
+
+}
