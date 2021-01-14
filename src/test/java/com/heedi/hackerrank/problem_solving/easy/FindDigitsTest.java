75013156d90f0ec483421e9339dcd6f8ca55ad1f
sefide <sefide@naver.com>
2021-01-14 23:24:41 +0900

    feat: solve Find Digits

---
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/FindDigitsTest.java
@@ -0,0 +1,29 @@
+package com.heedi.hackerrank.problem_solving.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * https://www.hackerrank.com/challenges/find-digits/problem
+ */
+public class FindDigitsTest {
+
+    @Test
+    void test() {
+        int n = 1012;
+        System.out.println(findDigits(n));
+    }
+
+    static int findDigits(int n) {
+        return (int) String.valueOf(n)
+                .chars()
+                .boxed()
+                .distinct()
+                .filter(i -> i != 0 && n % i == 0)
+                .count();
+    }
+}
