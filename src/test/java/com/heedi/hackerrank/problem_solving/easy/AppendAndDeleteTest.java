2b5e6e6dc788c675fb0d5c9166d46f9c0d1fa88e
sefide <sefide@naver.com>
2021-01-16 22:45:29 +0900

    feat: add two easy test

---
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/AppendAndDeleteTest.java
@@ -0,0 +1,22 @@
+package com.heedi.hackerrank.problem_solving.easy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://www.hackerrank.com/challenges/append-and-delete/problem
+ */
+public class AppendAndDeleteTest {
+
+    @Test
+    void test() {
+        String s = "aba";
+        String t = "aba";
+        int k = 7;
+        System.out.println(appendAndDelete(s, t, k));
+    }
+
+    static String appendAndDelete(String s, String t, int k) {
+
+        return "";
+    }
+}
---
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/CutTheStickTest.java
@@ -0,0 +1,26 @@
+package com.heedi.hackerrank.problem_solving.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * https://www.hackerrank.com/challenges/cut-the-sticks/problem
+ */
+public class CutTheStickTest {
+
+    @Test
+    void test() {
+        int[] arr = {};
+        int[] result = cutTheSticks(arr);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] cutTheSticks(int[] arr) {
+        int[] result = new int[arr.length];
+
+        return result;
+    }
+
+}
