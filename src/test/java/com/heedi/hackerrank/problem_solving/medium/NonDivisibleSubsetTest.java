0acf6d7610538fcc46496258e80409af34b1edf7
sefide <sefide@naver.com>
2021-01-18 23:09:14 +0900

    fix: Non-Divisible Subset

--- src/test/java/com/heedi/hackerrank/problem_solving/medium/NonDivisibleSubsetTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/medium/NonDivisibleSubsetTest.java
@@ -15,38 +15,42 @@ public class NonDivisibleSubsetTest {
 
     @Test
     void test() {
-        int k = 13;
-        List<Integer> s = Arrays.asList(2375782, 21836421, 2139842193, 2138723, 23816, 21836219, 2948784, 43864923, 283648327, 23874673);
-
+        int k = 7;
+//        List<Integer> s = Arrays.asList(2375782, 21836421, 2139842193, 2138723, 23816, 21836219, 2948784, 43864923, 283648327, 23874673); // k = 13
+//        List<Integer> s = Arrays.asList(1, 7, 2, 4); // k = 3
+        List<Integer> s = Arrays.asList(278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436); // k = 7
         System.out.println(nonDivisibleSubset(k, s));
     }
 
-    // TODO : fix
     public static int nonDivisibleSubset(int k, List<Integer> s) {
-        if (k == 1) {
-            return 1;
-        }
         Map<Integer, Long> remainders = s.stream()
                 .map(i -> i % k)
                 .collect(groupingBy(Function.identity(), Collectors.counting()));
 
+        int count = 0;
         Set<Integer> removed = new HashSet<>();
+
         for (int r : remainders.keySet()) {
-            int l = k - r;
-            if (remainders.get(l) != null) {
-                int remove = Math.min(remainders.get(l), remainders.get(r)) == remainders.get(l) ? l : r;
-                if (!removed.contains(k - remove)) {
+            if (r == (double)k / 2 || r == 0) {
+                count += 1;
+            } else if (!removed.contains(k - r) && !removed.contains(r)) {
+                int remove;
+
+                if(remainders.get(k - r) != null) {
+                    if(remainders.get(r) < remainders.get(k - r)) {
+                        remove = r;
+                    } else {
+                        remove = k - r;
+                    }
+                    count += remainders.get(k - remove);
                     removed.add(remove);
+                } else {
+                    count += remainders.get(r).intValue();
                 }
             }
         }
 
-        return Integer.parseInt(remainders.keySet()
-                .stream()
-                .filter(i -> !removed.contains(i))
-                .map(remainders::get)
-                .reduce(0L, Long::sum)
-                .toString());
+        return count;
     }
 
     /*
