6afc5fcd59d569be5fb216bfd511539ab2aeed76
sefide <sefide@naver.com>
2021-01-20 22:32:46 +0900

    feat: add ACM ICPC Team class

---
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/ACMICPCTeamTest.java
@@ -0,0 +1,25 @@
+package com.heedi.hackerrank.problem_solving.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * https://www.hackerrank.com/challenges/acm-icpc-team/problem
+ */
+public class ACMICPCTeamTest {
+
+    @Test
+    void test() {
+        String[] topic = {"10101", "11100", "11010", "00101"};
+        int[] result = acmTeam(topic);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] acmTeam(String[] topic) {
+
+        return null;
+    }
+
+}
