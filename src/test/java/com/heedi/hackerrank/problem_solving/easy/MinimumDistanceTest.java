9baa4959b2f6030a810a9198375fe5756a4466f6
sefide <sefide@naver.com>
2021-01-24 22:44:38 +0900

    refactor

--- src/test/java/com/heedi/hackerrank/problem_solving/easy/MinimumDistanceTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/MinimumDistanceTest.java
@@ -6,28 +6,26 @@ import org.junit.jupiter.api.Test;
  * https://www.hackerrank.com/challenges/minimum-distances/problem
  */
 public class MinimumDistanceTest {
+    private static final int NO_DISTANCE = -1;
 
     @Test
     void test() {
-        int[] a = {7, 1, 3, 4, 1, 7};
+        int[] a = {1, 2, 3, 4, 10};
 
         System.out.println(minimumDistances(a));
-
     }
 
     static int minimumDistances(int[] a) {
-        int distance = -1;
-
         int min = a.length;
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = i + 1; j < a.length; j++) {
-                if(a[i] == a[j] && j - i < min) {
+                if (a[i] == a[j] && j - i < min) {
                     min = j - i;
                 }
             }
         }
 
-        return min == a.length ? distance : min;
+        return min == a.length ? NO_DISTANCE : min;
     }
 
 }
