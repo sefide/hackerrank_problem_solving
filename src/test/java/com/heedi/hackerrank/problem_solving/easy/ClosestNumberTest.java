fba474e40c035db18498e23eda80c7dafb0f460c
sefide <sefide@naver.com>
2021-02-04 21:58:03 +0900

    feat: add link

--- src/test/java/com/heedi/hackerrank/problem_solving/easy/ClosestNumberTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/ClosestNumberTest.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * https://www.hackerrank.com/challenges/closest-numbers/problem
+ */
 public class ClosestNumberTest {
     @Test
     void test() {
