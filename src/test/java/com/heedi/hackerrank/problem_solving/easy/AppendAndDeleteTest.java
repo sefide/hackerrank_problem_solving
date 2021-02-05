17a712f8be626008027522aeca2b17a9ca8f848a
sefide <sefide@naver.com>
2021-02-05 22:12:25 +0900

    feat: solve Append And Delete

--- src/test/java/com/heedi/hackerrank/problem_solving/easy/AppendAndDeleteTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/AppendAndDeleteTest.java
@@ -2,10 +2,14 @@ package com.heedi.hackerrank.problem_solving.easy;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Year;
+
 /**
  * https://www.hackerrank.com/challenges/append-and-delete/problem
  */
 public class AppendAndDeleteTest {
+    private static final String YES = "Yes";
+    private static final String NO = "No";
 
     @Test
     void test() {
@@ -16,7 +20,26 @@ public class AppendAndDeleteTest {
     }
 
     static String appendAndDelete(String s, String t, int k) {
+        int sameCount = 0;
+        for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
+            if (s.charAt(i) == t.charAt(i)) {
+                sameCount++;
+            }
+        }
 
-        return "";
+        int shouldFixCount = (s.length() + t.length() - (2 * sameCount));
+
+        if (shouldFixCount > k) {
+            return NO;
+        }
+        else if (shouldFixCount % 2 == k % 2) {
+            return YES;
+        }
+        else if ((s.length() + t.length() - k) < 0) {
+            return YES;
+        }
+        else {
+            return NO;
+        }
     }
 }
