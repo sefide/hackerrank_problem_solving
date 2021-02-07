e64f35709c863e79a26b14b81c9445df8907d716
sefide <sefide@naver.com>
2021-02-07 21:04:52 +0900

    feat: add Jumping On The Clouds Test Class

--- src/test/java/com/heedi/hackerrank/problem_solving/easy/JumpingOnTheCloudsTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/JumpingOnTheCloudsTest.java
@@ -1,4 +1,22 @@
 package com.heedi.hackerrank.problem_solving.easy;
 
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+ */
 public class JumpingOnTheCloudsTest {
+
+    @Test
+    void test() {
+        int[] c = {};
+
+        System.out.println(jumpingOnClouds(c));
+    }
+
+    static int jumpingOnClouds(int[] c) {
+
+        return 0;
+    }
+
 }
