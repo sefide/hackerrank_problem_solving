658bb0f906e19952798d31eeba99a04f21b03e51
sefide <sefide@naver.com>
2021-02-07 20:56:56 +0900

    feat: change Test Name

--- src/test/java/com/heedi/hackerrank/problem_solving/easy/JumpingOnTheCloudsTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/JumpingOnTheCloudsTest.java
@@ -1,31 +1,4 @@
 package com.heedi.hackerrank.problem_solving.easy;
 
-import org.junit.jupiter.api.Test;
-
-/**
- * https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
- */
 public class JumpingOnTheCloudsTest {
-
-    @Test
-    void test() {
-        int[] c = {0, 0, 1, 0, 0, 1, 1, 0};
-        int k = 2;
-
-        System.out.println(jumpingOnClouds(c, k));
-    }
-
-    static int jumpingOnClouds(int[] c, int k) {
-        int n = c.length;
-        int i = 0;
-        int e = 100;
-
-        do {
-            e -= (c[(i + k) % n] * 2) + 1;
-            i = (i + k) % n;
-        } while (i != 0);
-
-        return e;
-    }
-
 }
