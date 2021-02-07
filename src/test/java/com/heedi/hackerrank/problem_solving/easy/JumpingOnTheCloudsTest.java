5604de624dfdd18ac32b4fe4d4d3bfba82ec3b2c
sefide <sefide@naver.com>
2021-02-07 21:27:38 +0900

    feat: solve Jumping On The Clouds

--- src/test/java/com/heedi/hackerrank/problem_solving/easy/JumpingOnTheCloudsTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/JumpingOnTheCloudsTest.java
@@ -9,14 +9,24 @@ public class JumpingOnTheCloudsTest {
 
     @Test
     void test() {
-        int[] c = {};
+//        int[] c = {0, 0, 1, 0, 0, 1, 0};
+        int[] c = {0, 0, 0, 1, 0, 0};
 
         System.out.println(jumpingOnClouds(c));
     }
 
     static int jumpingOnClouds(int[] c) {
+        int jump = 0;
 
-        return 0;
+        for (int i = 0; i < c.length - 1; jump++) {
+            if (i + 2 < c.length && c[i + 2] == 0) {
+                i += 2;
+            } else {
+                i += 1;
+            }
+        }
+
+        return jump;
     }
 
 }
