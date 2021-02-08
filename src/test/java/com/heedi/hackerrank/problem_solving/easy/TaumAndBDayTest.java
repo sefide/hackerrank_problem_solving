86bf70587e641b7cd6f1c3878a623e140b5f7d5e
sefide <sefide@naver.com>
2021-02-08 22:30:25 +0900

    feat: solve Taum And B'day Test

--- src/test/java/com/heedi/hackerrank/problem_solving/easy/TaumAndBDayTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/TaumAndBDayTest.java
@@ -10,5 +10,25 @@ public class TaumAndBDayTest {
     @Test
     void test() {
         // Taum and B'day , 25, 89.40%
+        int b = 3;
+        int w = 6;
+        int bc = 1;
+        int wc = 1;
+        int z = 1;
+
+        System.out.println(taumBday(b, w, bc, wc, z));
     }
+
+    public static long taumBday(int b, int w, int bc, int wc, int z) {
+        if (bc > wc + z) {
+            return (b + w) * wc + b * z;
+        }
+
+        if (wc > bc + z) {
+            return (b + w) * bc + w * z;
+        }
+
+        return (b * bc) + (w * wc);
+    }
+
 }
