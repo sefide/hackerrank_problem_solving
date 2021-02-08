9ef58df3c1f9905e26e9b8ca2f5cd7485b248ee3
sefide <sefide@naver.com>
2021-02-08 21:59:36 +0900

    feat: add Taum And B'day Test Class

---
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/TaumAndBDayTest.java
@@ -0,0 +1,14 @@
+package com.heedi.hackerrank.problem_solving.easy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://www.hackerrank.com/challenges/taum-and-bday/problem
+ */
+public class TaumAndBDayTest {
+
+    @Test
+    void test() {
+        // Taum and B'day , 25, 89.40%
+    }
+}
