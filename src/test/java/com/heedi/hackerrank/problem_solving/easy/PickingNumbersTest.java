bba9c7746f3ff7ac3195893b880802d61a1e33b6
sefide <sefide@naver.com>
2021-03-14 23:09:35 +0900

    feat: add link

--- src/test/java/com/heedi/hackerrank/problem_solving/easy/PickingNumbersTest.java
+++ src/test/java/com/heedi/hackerrank/problem_solving/easy/PickingNumbersTest.java
@@ -7,6 +7,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * https://www.hackerrank.com/challenges/picking-numbers/problem
+ */
 public class PickingNumbersTest {
 
     @Test
